3db0552aefa38901026e28459f9cf431a78e8538
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-14 11:38:16 -0500

    Adding Anonymous classes, anonymous functions, functional interfaces, and lambda expressions.

---
+++ src/W9/HelloWorld.java
@@ -0,0 +1,10 @@
+package W9;
+
+public class HelloWorld {
+	String name = "Hello";
+	
+	public String greeting() {
+		return this.name;
+	}
+
+}
--- src/W9/Main.java
+++ src/W9/Main.java
@@ -1,10 +1,41 @@
 package W9;
 
-public class Main {
+//	Introducing lambdas
+
+@FunctionalInterface
+interface Operation {
+	boolean isMin(int a, int b);	//	declare function signature using interface
+}
+
+public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Operation fi = (a, b) -> { return a < b; };	//	implement logic here
+		System.out.println(fi.isMin(5, 4));	//	isLesser
+		
+		HelloWorld hw = new HelloWorld() {
+			
+			String name = "toute le monde";
+			
+			public String greeting() {
+				return this.name;
+			}
+		};
+		
+		System.out.println(bar(1, hw));
+		System.out.println(foo(5, 4, (x, y) -> { return x > y; }));	//	isGreater
+		
 	}
 
+	
+	public static String bar(int x, HelloWorld hw) {
+		return hw.greeting();
+	}
+	
+	//	Setting Functional interface as parameters
+	public static boolean foo(int a, int b, Operation o) {
+		return o.isMin(a, b);
+	}
 }
