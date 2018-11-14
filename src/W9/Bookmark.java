77d24bc31e8b2680e1cba7a073e28988d9bf02b4
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-14 02:24:27 -0500

    Fixing cluttered classes

--- src/W9/Bookmark.java
+++ src/W9/Bookmark.java
@@ -1,22 +1,35 @@
 package W9;
 
 public class Bookmark {
-	 private long id;
-	 private String title;
-	 
-	 public long getId() {
-	  return id;
-	 }
-	 
-	 public void setId(long id) {
-	  this.id = id;
-	 }
-	 
-	 public String getTitle() {
-	  return title;
-	 }
-	 
-	 public void setTitle(String title) {
-	  this.title = title;
-	 }
+	private long id;
+	private String title;
+	// New 
+	private double rating;
+		
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	//New
+	public double getRating() {
+		return rating;
+	}
+
+	//New
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+		
 }
